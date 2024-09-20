15992353660938c064b5ebe32173f2bd168f3d8b
Christopher Abate <contact@christopherabate.com>
2024-09-20 17:04:03 +0200

    Add files via upload

--- assets/js/accname.js
+++ assets/js/accname.js
@@ -1,31 +1,35 @@
 export function accName(element) {
   const isVisible = (element) => {
-    while (element) {
-      const style = getComputedStyle(element);
-      if (style.display === "none" || style.visibility === "hidden") {
-        return false;
-      }
-      element = element.parentElement;
-    }
-    return true;
+    if (!element) return true;
+    return (getComputedStyle(element).visibility !== 'hidden' && getComputedStyle(element).display !== 'none' && !element.closest('[aria-hidden="true"]'))
+    ? isVisible(element.parentElement)
+    : false;
   };
+  
+  const getVisibleContent = (element) => {
+    return isVisible(element)
+    ? [[...element.childNodes].filter(node => node.nodeType === Node.TEXT_NODE).map(node => node.textContent.trim()).join(' '),[...element.children].map(getVisibleContent).filter(Boolean).join(' ')]
+      .join(' ')
+      .trim()
+    : '';
+  }
 
   const getVisibleLabels = (selector) => [...element.ownerDocument.querySelectorAll(selector)]
-  .filter(label => label && isVisible(label) && !label.closest('[aria-hidden="true"]'))
+  .filter(label => label && isVisible(label))
   .map(label => label.textContent.trim())
-  .join(", ");
+  .join(', ');
 
-  const ariaLabels = getVisibleLabels(`#${element.getAttribute("aria-labelledby")?.split(' ').join(', #')}`);
-  const ariaLabel = element.getAttribute("aria-label")?.trim();
+  const ariaLabels = getVisibleLabels(`#${element.getAttribute('aria-labelledby')?.split(' ').join(', #')}`);
+  const ariaLabel = element.getAttribute('aria-label')?.trim();
   const labelForElement = getVisibleLabels(`label[for="${element.id}"]`);
-  const closestLabel = element.closest("label")?.textContent.trim();
-  const altText = element.hasAttribute("alt") ? element.getAttribute("alt")?.trim() : null;
-  const figcaption = element.closest("figure")?.querySelector("figcaption")?.textContent.trim();
-  const caption = element.closest("table")?.querySelector("caption")?.textContent.trim();
-  const legend = element.closest("fieldset")?.querySelector("legend")?.textContent.trim();
-  const title = element.getAttribute("title")?.trim();
-  const placeholder = element.getAttribute("placeholder")?.trim();
-  const textContent = element.textContent?.trim();
+  const closestLabel = element.closest('label')?.textContent.trim();
+  const altText = element.hasAttribute('alt') ? element.getAttribute('alt')?.trim() : null;
+  const figcaption = element.closest('figure')?.querySelector('figcaption')?.textContent.trim();
+  const caption = element.closest('table')?.querySelector('caption')?.textContent.trim();
+  const legend = element.closest('fieldset')?.querySelector('legend')?.textContent.trim();
+  const title = element.getAttribute('title')?.trim();
+  const placeholder = element.getAttribute('placeholder')?.trim();
+  const textContent = getVisibleContent(element)?.trim();
 
-  return ariaLabels || ariaLabel || labelForElement || closestLabel || altText || figcaption || caption || legend || title || placeholder || textContent || "";
+  return ariaLabels || ariaLabel || labelForElement || closestLabel || altText || figcaption || caption || legend || title || placeholder || textContent || '';
 }
--- assets/js/screenreader.js
+++ assets/js/screenreader.js
@@ -14,7 +14,7 @@ export class ScreenReader {
   }
   
   // Collect elements from all elements or a subset
-  collect(subset = "") {
+  collect(subset = '') {
     return [...this.screen.contentWindow.document.querySelectorAll(Object.keys(subset ? this.elements[subset] : Object.assign({}, ...Object.values(this.elements))).toString())]
     .filter(element => {
       while (element) {
@@ -30,10 +30,10 @@ export class ScreenReader {
   }
   
   // Update then return instance 
-  move({ list = "", reverse = false } = {}) {
+  move({ list = '', reverse = false } = {}) {
     // Find the next matching element after/before the current index
-    const nextElement = (reverse ? this.readable.slice(0, this.current) : this.readable.slice(this.current + 1))[reverse ? "findLast" : "find"]((element) => element.matches(Object.keys(list ? this.elements[list] : Object.assign({}, ...Object.values(this.elements))).toString()));
-    this.current = nextElement ? this.readable.indexOf(nextElement) : this.readable[reverse ? "findLastIndex" : "findIndex"]((element) => element.matches(Object.keys(list ? this.elements[list] : Object.assign({}, ...Object.values(this.elements))).toString()));
+    const nextElement = (reverse ? this.readable.slice(0, this.current) : this.readable.slice(this.current + 1))[reverse ? 'findLast' : 'find']((element) => element.matches(Object.keys(list ? this.elements[list] : Object.assign({}, ...Object.values(this.elements))).toString()));
+    this.current = nextElement ? this.readable.indexOf(nextElement) : this.readable[reverse ? 'findLastIndex' : 'findIndex']((element) => element.matches(Object.keys(list ? this.elements[list] : Object.assign({}, ...Object.values(this.elements))).toString()));
     return this;
   }
   
@@ -51,13 +51,13 @@ export class ScreenReader {
   // Return current element name
   speak({ wrapper, element = this.readable[this.current] } = {}) {
     return `${Object.assign({}, ...Object.values(this.elements))?.[element.tagName]} "${(wrapper ? wrapper(element) : element.textContent?.trim()) || 'vide'}"`
-    + (element.selectedOptions ? ` ${[...element.selectedOptions].map(option => option.label).join(', ')}` : "")
-    + (element.value ? ` : ${element.value.trim()}` : "")
-    + (element.src && !element.hasAttribute("alt") ? ` (${element.src})` : "")
-    + (element.getAttribute("placeholder") ? ` (${element.getAttribute("placeholder")})` : "")
-    + (element.required ? " (Obligatoire)" : "")
-    + (element.checked ? " (Coché)" : "")
-    + (element.selected ? " (Sélectionné)" : "")
-    + (element.disabled ? " (Désactivé)" : "")
+    + (element.selectedOptions ? ` ${[...element.selectedOptions].map(option => option.label).join(', ')}` : '')
+    + (element.value ? ` : ${element.value.trim()}` : '')
+    + (element.hasAttribute('alt') ? (element.getAttribute('alt') ? '' : ' Décorative') : (element.getAttribute('src') ? ` ${element.getAttribute('src').split('/').pop()}` : ''))
+    + (element.getAttribute('placeholder') ? ` (${element.getAttribute('placeholder')})` : '')
+    + (element.required ? ' (Obligatoire)' : '')
+    + (element.checked ? ' (Coché)' : '')
+    + (element.selected ? ' (Sélectionné)' : '')
+    + (element.disabled ? ' (Désactivé)' : '');
   }
 }
